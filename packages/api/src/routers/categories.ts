9b4f6379bbd692a0790dc40568963ad4ececaa38
Lev Tyulev <lev@example.com>
2025-10-29 19:47:21 +0300

    categories api

---
+++ packages/api/src/routers/categories.ts
@@ -0,0 +1,13 @@
+import z from "zod";
+import prisma from "@ithub-shoes/db";
+import { publicProcedure } from "../index";
+
+export const categoriesRouter = {
+	getAll: publicProcedure.handler(async () => {
+		return await prisma.todo.findMany({
+			orderBy: {
+				id: "asc",
+			},
+		});
+	}),
+};
