17c8b6fedcb285dd61ef4d3b3947dd515019fa91
Трофимов Алексей <a.trofimov@domain.local>
2025-11-05 19:45:18 +0300

    wip: zod integration

--- packages/api/src/routers/categories.ts
+++ packages/api/src/routers/categories.ts
@@ -1,13 +1,51 @@
 import z from "zod";
+import { implement } from "@orpc/server";
+import { oc } from "@orpc/contract";
+
 import prisma from "@ithub-shoes/db";
 import { publicProcedure } from "../index";
 
+
+const idInputSchema = z.object({
+	id: z.string().transform(value => Number(value))
+})
+
+const categorySchema = z.object({
+	id: z.int().min(1),
+	category_name: z.string()
+})
+
+const allCategoriesSchema = z.array(categorySchema)
+
+const categoriesContract = {
+	getAll: oc
+		.route({
+			path: "/categories",
+			summary: "Получить все категории",
+		})
+		.output(allCategoriesSchema),
+	getOne: oc
+		.route({
+			path: "/categories/{id}",
+			summary: "Получить одну категорию"
+		})
+		.input(idInputSchema)
+		.output(categorySchema)
+}
+
 export const categoriesRouter = {
-	getAll: publicProcedure.handler(async () => {
+	getAll: implement(categoriesContract.getAll).handler(async () => {
 		return await prisma.category.findMany({
 			orderBy: {
 				id: "asc",
 			},
 		});
 	}),
+	getOne: implement(categoriesContract.getOne).handler(async ({ input }) => {
+		return await prisma.category.findUnique({
+			where: {
+				id: input.id
+			}
+		});
+	})
 };
