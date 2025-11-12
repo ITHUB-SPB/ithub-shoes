ab708116016047998f2e5dc9641f8769dfa29e76
Bokov Roman <romabokov191@gmail.com>
2025-11-12 18:20:02 +0300

    commit

--- packages/api/src/routers/categories.ts
+++ packages/api/src/routers/categories.ts
@@ -1,13 +1,54 @@
 import z from "zod";
 import prisma from "@ithub-shoes/db";
 import { publicProcedure } from "../index";
+import { oc } from "@orpc/contract";
+import { implement } from "@orpc/server";
+
+
+const idInputSchema = z.object({
+	id: z.string().refine(value => Number(value))
+})
+
+const categorySchema = z.object({
+  id: z.int().min(1),
+  category_name: z.string(),
+});
+
+
+const allCategoriesSchema = z.array(z.object({
+	id: z.int().min(1),
+	category_name: z.string(),
+}))
+
+const categoriesContact = {
+  getAll: oc
+    .route({
+      path: "/categories",
+      summary: "получить отсортированный список",
+    })
+    .output(allCategoriesSchema),
+  getOne: oc
+    .route({
+      path: "/categories/{id}",
+      summary: "получить одну категорию",
+	})
+	.input(idInputSchema)
+  .output(categorySchema),
+};
 
 export const categoriesRouter = {
-	getAll: publicProcedure.handler(async () => {
+  getAll: implement(categoriesContact.getAll).handler(async () => {
+    return await prisma.category.findMany({
+      orderBy: {
+        id: "asc",
+      },
+    });
+  }),
+	getOne: implement(categoriesContact.getOne).handler(async ({input}) => {
 		return await prisma.category.findMany({
-			orderBy: {
-				id: "asc",
-			},
-		});
-	}),
+			where: {
+			id: input.id
+		}
+    });
+  }),
 };
